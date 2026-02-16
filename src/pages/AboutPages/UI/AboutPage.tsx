93e2bfdeeb20b5988cf4aff8d6d7ee5471784b9e
Dias Nurgaliyev <56669473+diz1l@users.noreply.github.com>
2026-02-16 06:01:31 +0100

    Resolve merge conflict

--- src/pages/AboutPages/UI/AboutPage.tsx
+++ src/pages/AboutPages/UI/AboutPage.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
         My name is Dias. I am 23 years old, from Kazakhstan, living in Prague.
       </p>
       <p className="about-text">
-        I have been learning frontend development for 3 year. I am looking for a job as a frontend developer.
+        I have been learning frontend development for 3 years. I am looking for a job as a frontend developer.
       </p>
       <p className="about-text">
         I am ready to work hard and learn new things.
